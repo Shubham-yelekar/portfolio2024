355b700ec5ab874fd64444fc42af83f21493105f
Shubham Yelekar <70461738+Shubham-yelekar@users.noreply.github.com>
2024-07-18 23:57:34 +0530

    project grid

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='p-4'>
+      <div className='title-wrapper'>
+        <h2>Projects</h2>
+        <p>These are all my recent works</p>
+      </div>
+      <div className='projects-wrapper'>
+        <div className='bg-gray-800 h-[620px] md:h-[520px]'></div>
+        <div className='bg-gray-800 h-[620px] md:h-[520px]'></div>
+      </div>
+    </div>
   )
 }
 
