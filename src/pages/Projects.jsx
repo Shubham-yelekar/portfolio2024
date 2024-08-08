cc30f7f03dbdbff9bdb7c6f8a01a4061e3e8b9d7
shubham <shubhamyelekar0@gmail.com>
2024-08-08 15:20:13 +0530

    background for projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,6 +14,7 @@ const projects = [
 const Projects = () => {
   return (
     <motion.div variants={textVariants} initial="initial" animate="animate" className='p-4'>
+      <motion.div className='top-blob-projects'></motion.div>
       <motion.div variants={childVariants} className='title-wrapper'>
         <h2>Projects</h2>
         <p>Websites, software interfaces, icons — that’s where my heart is,dedicating my skills to perfecting these realms.</p>
