a925862d534ed2db718b225d7af0171bf7eabb42
shubham <shubhamyelekar0@gmail.com>
2024-08-28 15:47:54 +0530

    map added intro added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,25 +11,24 @@ const About = () => {
       <div className='about-grid-wrapper'>
 
         <div className='bento-card intro-box'>
-          <h3>I like to build what i design,</h3>
-          {/* <p>Been diving into the visual vibe for over ten years, always on a mission to make things look awesome, one pixel at a time.</p>
-          <p>Websites, software interfaces, icons — that’s where my heart is, dedicating my skills to perfecting these realms.</p>
-          <p>My curiosity's like a never-ending fuel, pushing me to polish my skills and make sure everything I touch turns into digital gold.</p> */}
+          <h3>"I like to build what i design,"</h3>
+          <p>I love bringing my designs to life, turning ideas into interactive, user-friendly experiences.</p>
+          <p>I’m passionate about using technology to make human-computer interactions better.</p>
+          <p>My goal is to create UIs that are not only visually appealing but also practical and easy to use. </p>
        </div>
 
        <div className='bento-card profile-img-box'></div>
 
        <div className='bento-card experience-box'><h5>Experience</h5></div>
-       <div className='bento-card map-box'><h5>Map</h5></div>
+       <div className='bento-card map-box'>
+        <div className='map-marking'></div>
+       </div>
 
        <div className='bento-card x-box'><h5>X</h5></div>
        <div className='bento-card linkdin-box'><h5>Linkedin</h5></div>
        <div className='bento-card tech-stack-box'><h5>Tech Stack</h5></div>
        <div className='bento-card tool-box'><h5>Tool box</h5></div>
        <div className='bento-card experimental-box'><h5>Experimantal</h5></div>
-       <div className='bento-card more-about-box'><h5>More About</h5></div>
-       <div className='bento-card hobby-box'><h5>Hobby</h5></div>
-       <div className='bento-card bucket-box'><h5>Bucket List</h5></div>
        <div className='bento-card thankyou-boy'><h5>Thank you</h5></div>
       </div>
       <Footer/>
