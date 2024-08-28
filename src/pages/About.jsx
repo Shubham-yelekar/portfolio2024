a70b4b4542d0cb56462425cbf93c1879c95f3898
shubham <shubhamyelekar0@gmail.com>
2024-08-28 17:52:50 +0530

    about section settings

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,36 +1,41 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 import Footer from '../components/Footer'
-
+import textVariants from '../static/textVariants'
+import childVariants from '../static/childVariants'
+import Linkedin from '../icons/Linkedin'
+import XDotCom from '../icons/XDotCom'
 
 const About = () => {
   return (
     <motion.div className='p-2 sm:p-4'>
       <motion.div className='top-blob-about'></motion.div>
       <motion.div className='top-grid-about'></motion.div>
-      <div className='about-grid-wrapper'>
+      <motion.div  variants={textVariants} initial='initial' animate='animate'  className='about-grid-wrapper'>
 
-        <div className='bento-card intro-box'>
-          <h3>"I like to build what i design,"</h3>
+        <motion.div variants={childVariants} className='bento-card intro-box'>
+          <motion.h6 variants={childVariants}>"I like to build what i design,"</motion.h6>
           <p>I love bringing my designs to life, turning ideas into interactive, user-friendly experiences.</p>
           <p>I’m passionate about using technology to make human-computer interactions better.</p>
           <p>My goal is to create UIs that are not only visually appealing but also practical and easy to use. </p>
-       </div>
+       </motion.div>
 
        <div className='bento-card profile-img-box'></div>
 
-       <div className='bento-card experience-box'><h5>Experience</h5></div>
+       <div className='bento-card experience-box'><h6>Experience</h6></div>
        <div className='bento-card map-box'>
         <div className='map-marking'></div>
        </div>
 
-       <div className='bento-card x-box'><h5>X</h5></div>
-       <div className='bento-card linkdin-box'><h5>Linkedin</h5></div>
-       <div className='bento-card tech-stack-box'><h5>Tech Stack</h5></div>
-       <div className='bento-card tool-box'><h5>Tool box</h5></div>
-       <div className='bento-card experimental-box'><h5>Experimantal</h5></div>
-       <div className='bento-card thankyou-boy'><h5>Thank you</h5></div>
-      </div>
+       <div className='bento-card x-box'><XDotCom/></div>
+       <div className='bento-card linkdin-box'><Linkedin/></div>
+       <div className='bento-card tech-stack-box'><h6>Tech Stack</h6></div>
+       <div className='bento-card tool-box'><h6>Tool box</h6></div>
+       <div className='bento-card experimental-box'><h6>Experimantal</h6></div>
+       <div className='bento-card thankyou-boy'><h6>Thank you</h6></div>
+
+      </motion.div>
+
       <Footer/>
       </motion.div>
   )
