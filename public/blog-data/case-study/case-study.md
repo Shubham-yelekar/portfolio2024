017ce80d93720ab364ed6ad840a4581af7e96189
shubham <shubhamyelekar0@gmail.com>
2024-11-15 20:19:16 +0530

    markdown js added . experimented with json github hosting

---
+++ public/blog-data/case-study/case-study.md
@@ -0,0 +1,33 @@
+# Case Title [Template]
+
+Client: Company name LLC
+Industry: Fintech
+Category: Mobile app, Web app, Website
+Duration: December 1, 2023 → February 29, 2024
+Location: USA
+
+## Properties
+
+<p>
+💡 Creating case studies is an essential aspect of presenting your expertise and success stories in a structured and compelling way. There are certain properies to follow when creating a case study.
+
+</p>
+
+![Image.png](/public/blog-data//case-study/Image.png)
+
+
+## Business needs
+
+<ul>
+        <li>**Title:** [User Engagement Increase]</li>
+        <li>**Number:** [50% higher]</li>
+</ul>
+
+
+
+
+
+
+
+
+
--- src/pages/Archives.jsx
+++ src/pages/Archives.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 import {motion} from 'framer-motion'
 import textVariants from '../static/textVariants';
@@ -7,12 +7,28 @@ import { BiLinkExternal } from "react-icons/bi";
 
 
 
-const Projects = [
-  {id:1, name: 'Shiny Animated Button',description:"React.js , Tailwind CSS & Framer Motion  ", thumbnail: '/assets/archives/archive-1/thumb-1.jpg', video: '/assets/archives/shiny-button/shiny-button.mp4', linkType: 'Demo',link: 'https://shiny-animated-button.netlify.app/'},
+// const Projects = [
+//   {id:1, name: 'Shiny Animated Button',description:"React.js , Tailwind CSS & Framer Motion  ", thumbnail: '/assets/archives/archive-1/thumb-1.jpg', video: '/assets/archives/shiny-button/shiny-button.mp4', linkType: 'Demo',link: 'https://shiny-animated-button.netlify.app/'},
 
-]
+// ]
 
 const Archives = () => {
+
+  const [archivePost , setArchivePost] = useState([])
+
+  useEffect(() => {
+    fetch('https://shubham-yelekar.github.io/projects-json/projects.json')
+      .then(res => res.json())
+      .then(data => {
+      setArchivePost(data.archive)
+        console.log(archivePost)
+      })
+
+      .catch(error => console.error("Error fetch json", error))
+
+  }, [])
+
+
   return (
     <motion.div variants={textVariants} initial="initial" animate="animate" className='p-4'>
       <motion.div className='top-blob-archive'></motion.div>
@@ -24,7 +40,7 @@ const Archives = () => {
 
       <div className='archive-wrapper'>
 
-          {Projects.map((items)=>(
+          {archivePost.map((items)=>(
             <div key={items.id} className='archive-card'>
               {items.video ? (
                 <video autoPlay muted playsInline loop src={items.video} alt={items.name}></video>
