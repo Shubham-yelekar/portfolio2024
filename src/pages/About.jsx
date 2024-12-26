4c969d9f867b3cbef44f21b353f618b8e0628512
shubham <shubhamyelekar0@gmail.com>
2024-12-26 13:04:48 +0530

    about section correction

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import {motion} from 'framer-motion'
-import Footer from '../components/Footer'
-import textVariants from '../static/textVariants'
-import childVariants from '../static/childVariants'
-import Linkedin from '../icons/Linkedin'
-import XDotCom from '../icons/XDotCom'
+import React from "react";
+import { motion } from "framer-motion";
+import Footer from "../components/Footer";
+import textVariants from "../static/textVariants";
+import childVariants from "../static/childVariants";
+import Linkedin from "../icons/Linkedin";
+import XDotCom from "../icons/XDotCom";
 import { MdWork } from "react-icons/md";
 
 import { FaTools } from "react-icons/fa";
@@ -12,209 +12,227 @@ import { HiDocumentText } from "react-icons/hi2";
 
 import { PiHandsPrayingBold } from "react-icons/pi";
 
-
-
-
 const design = [
   {
-    name: 'Figma',
-    logo: '/assets/various/figma-logo.png',
-    desc: 'Design & Prototyiping'
+    name: "Figma",
+    logo: "/assets/various/figma-logo.png",
+    desc: "Design & Prototyiping",
   },
   {
-    name: 'Spline',
-    logo: '/assets/various/spline-logo.png',
-    desc: '3d Editor'
+    name: "Spline",
+    logo: "/assets/various/spline-logo.png",
+    desc: "3d Editor",
   },
   {
-    name: 'Illustrator',
-    logo: '/assets/various/ai-logo.png',
-    desc: 'Vector Design Tool'
+    name: "Illustrator",
+    logo: "/assets/various/ai-logo.png",
+    desc: "Vector Design Tool",
   },
   {
-    name: 'Photoshop',
-    logo: '/assets/various/ps-logo.png',
-    desc: 'Image Editor'
+    name: "Photoshop",
+    logo: "/assets/various/ps-logo.png",
+    desc: "Image Editor",
   },
-]
+];
 
 const webDev = [
   {
-    name: 'JavaScript',
-    logo: '/assets/various/js-logo.png',
-    desc: 'Languages of the web'
+    name: "JavaScript",
+    logo: "/assets/various/js-logo.png",
+    desc: "Languages of the web",
   },
   {
-    name: 'ReactJS',
-    logo: '/assets/various/react-logo.png',
-    desc: 'A JavaScript Library'
+    name: "ReactJS",
+    logo: "/assets/various/react-logo.png",
+    desc: "A JavaScript Library",
   },
   {
-    name: 'Tailwind & SASS',
-    logo: '/assets/various/tailwind-logo.png',
-    desc: 'CSS Frameworks'
+    name: "Tailwind & SASS",
+    logo: "/assets/various/tailwind-logo.png",
+    desc: "CSS Frameworks",
   },
   {
-    name: 'NextJS',
-    logo: '/assets/various/next-logo.svg',
-    desc: 'React Framework'
+    name: "NextJS",
+    logo: "/assets/various/next-logo.svg",
+    desc: "React Framework",
   },
   {
-    name: 'Angular',
-    logo: '/assets/various/angular-logo.png',
-    desc: 'JavaScript Framework'
+    name: "Angular",
+    logo: "/assets/various/angular-logo.png",
+    desc: "JavaScript Framework",
   },
   {
-    name: 'Redux',
-    logo: '/assets/various/redux-logo.png',
-    desc: 'State Managment'
+    name: "Redux",
+    logo: "/assets/various/redux-logo.png",
+    desc: "State Managment",
   },
   {
-    name: 'Git & GitHub',
-    logo: '/assets/various/github-logo.png',
-    desc: 'Version Control'
+    name: "Git & GitHub",
+    logo: "/assets/various/github-logo.png",
+    desc: "Version Control",
   },
   {
-    name: 'Appwrite',
-    logo: '/assets/various/appwrite-logo.png',
-    desc: 'Backend tool'
+    name: "Appwrite",
+    logo: "/assets/various/appwrite-logo.png",
+    desc: "Backend tool",
   },
   {
-    name: 'Supabase',
-    logo: '/assets/various/supabase-logo.png',
-    desc: 'Backend tool'
+    name: "Supabase",
+    logo: "/assets/various/supabase-logo.png",
+    desc: "Backend tool",
   },
-]
+];
 
 const handleOpenResume = () => {
   // Replace 'your-resume.pdf' with the actual path to your resume file
-  const resumeUrl = "https://drive.google.com/file/d/1gC-Gew05dzo0GINfC_zPwtVbggFt2adR/view?usp=sharing";
+  const resumeUrl =
+    "https://drive.google.com/file/d/1gC-Gew05dzo0GINfC_zPwtVbggFt2adR/view?usp=sharing";
   window.open(resumeUrl, "_blank");
 };
 
 const About = () => {
-
   return (
-    <motion.div className='p-2 sm:p-4'>
-      <motion.div className='top-blob-about'></motion.div>
-      <motion.div className='top-grid-about'></motion.div>
-      <motion.div  variants={textVariants} initial='initial' animate='animate'  className='about-grid-wrapper'>
-
-      <motion.div  variants={textVariants} initial='initial' animate='animate' className='flex flex-col gap-10'>
-
-        <motion.div variants={childVariants} className='bento-card '>
-          <p className='mb-4'>Hey, I’m <span className='about-text-span'>Shubham Yelekar</span>, a Front-end engineer who enjoys designing and coding.
-          </p>
-          <p className='mb-4'>I focus on creating easy-to-use and visually pleasing interfaces. I’m currently working at <span className='about-text-span'>Artificial Reality</span>, where I turn my designs into working applications.</p>
-          <p>I like using tools like React.js, Next.js, and Figma to bring ideas to life.</p>
-
-       </motion.div>
-
-      </motion.div>
-
-       <div className='bento-card profile-img-box'>
-
-       </div>
-
-       <div className='flex flex-col sm:flex-row  bento-card gap-8 '>
-          <div className='map-box'>
-            <div className='map-marking'></div>
-
-          </div>
-          <div className='about-image-2'>
-
-          </div>
-          <motion.div variants={childVariants} className='bento-card intro-box'>
-          <div className='about-image-1'>
-          </div>
-        </motion.div>
-        </div>
-
-        <div>
-        <button
-          onClick={handleOpenResume}
-          className="resume-button"
+    <motion.div className="p-2 sm:p-4">
+      <motion.div className="top-blob-about"></motion.div>
+      <motion.div className="top-grid-about"></motion.div>
+      <motion.div
+        variants={textVariants}
+        initial="initial"
+        animate="animate"
+        className="about-grid-wrapper"
+      >
+        <motion.div
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+          className=" grid grid-cols-1 grid-rows-2 gap-4 sm:grid-cols-3 sm:grid-rows-1 "
         >
-          View Resume <HiDocumentText size={18}  />
-        </button>
+          <motion.div className="relative about-image-1">
+            <button onClick={handleOpenResume} className="resume-button">
+              View Resume <HiDocumentText size={18} />
+            </button>
+          </motion.div>
+
+          <motion.div
+            variants={childVariants}
+            className="bento-card row-span-2 sm:col-span-2"
+          >
+            <p className="mb-4">
+              Hey, I’m <span className="about-text-span">Shubham Yelekar</span>,
+              a Front-end engineer who enjoys designing and coding.
+            </p>
+            <p className="mb-4">
+              I focus on creating easy-to-use and visually pleasing interfaces.
+              I’m currently working at{" "}
+              <span className="about-text-span">Artificial Reality</span>, where
+              I turn my designs into working applications.
+            </p>
+            <p>
+              I like using tools like React.js, Next.js, and Figma to bring
+              ideas to life.
+            </p>
+          </motion.div>
+        </motion.div>
+
+        {/* <div className="bento-card profile-img-box"></div> */}
+
+        <div className="flex flex-col sm:flex-row  bento-card gap-8 ">
+          <div className="map-box">
+            <div className="map-marking"></div>
+          </div>
+          <div className="about-image-2"></div>
         </div>
 
-
-       <div className='bento-card experience-box'>
-        <div className='flex text-[24px]  gap-4 items-center about-title'> <MdWork  size={18}/> <h6 className='text-[16px]'>Experience</h6> </div>
-        <div className='flex flex-col experience-list'>
-          <div className='experience-list-item'>
-            <h5>Front-end Developer <span> ＠ Artificial Reality</span> </h5>
-            <h6>➜ 2023 ―― Present</h6>
+        <div className="bento-card experience-box">
+          <div className="flex text-[24px]  gap-4 items-center about-title">
+            {" "}
+            <MdWork size={18} /> <h6 className="text-[16px]">Experience</h6>{" "}
           </div>
-          <div className='experience-list-item'>
-            <h5>Web Developer & Designer <span> ＠ Freelance</span> </h5>
-            <h6>➜ 2022 ―― 2023</h6>
-          </div>
-          <div className='experience-list-item'>
-            <h5>Electronics Engineer<span> ＠ R.A.I.T., Navi Mumbai  </span> </h5>
-            <h6>➜ passout 2022</h6>
+          <div className="flex flex-col experience-list">
+            <div className="experience-list-item">
+              <h5>
+                Front-end Developer <span> ＠ Artificial Reality</span>{" "}
+              </h5>
+              <h6>➜ 2023 ―― Present</h6>
+            </div>
+            <div className="experience-list-item">
+              <h5>
+                Web Developer & Designer <span> ＠ Freelance</span>{" "}
+              </h5>
+              <h6>➜ 2022 ―― 2023</h6>
+            </div>
+            <div className="experience-list-item">
+              <h5>
+                Electronics Engineer<span> ＠ R.A.I.T., Navi Mumbai </span>{" "}
+              </h5>
+              <h6>➜ passout 2022</h6>
+            </div>
           </div>
         </div>
 
-        </div>
-
-
-       <div className='bento-card tools-box'>
-         <div className='flex  gap-4 items-center  about-title'> <FaTools size={18}/> <h6 className='text-[16px]' >Technologies and Tool Box</h6>
-         </div>
-         <div className='mt-8'>
+        <div className="bento-card tools-box">
+          <div className="flex  gap-4 items-center  about-title">
+            {" "}
+            <FaTools size={18} />{" "}
+            <h6 className="text-[16px]">Technologies and Tool Box</h6>
+          </div>
+          <div className="mt-8">
             <h4>Web - Development</h4>
-            <div className='mt-8 tech-box-grid'>
-
-              {webDev.map((box, index)=>(
-              <div key={index} className='tech-box flex gap-4'>
-                <div className='logo-box'>
-                  <img src={box.logo} alt="" />
+            <div className="mt-8 tech-box-grid">
+              {webDev.map((box, index) => (
+                <div key={index} className="tech-box flex gap-4">
+                  <div className="logo-box">
+                    <img src={box.logo} alt="" />
+                  </div>
+                  <div>
+                    <h6>{box.name}</h6>
+                    <p>{box.desc}</p>
+                  </div>
                 </div>
-                <div>
-                  <h6>{box.name}</h6>
-                  <p>{box.desc}</p>
-                </div>
-              </div>
               ))}
-
             </div>
-         </div>
+          </div>
 
-
-         <div className='mt-8'>
+          <div className="mt-8">
             <h4>UI Design</h4>
-            <div className='mt-8 tech-box-grid'>
-
-              {design.map((box, index)=>(
-              <div key={index} className='tech-box flex gap-4'>
-                <div className='logo-box'>
-                  <img src={box.logo} alt="" />
+            <div className="mt-8 tech-box-grid">
+              {design.map((box, index) => (
+                <div key={index} className="tech-box flex gap-4">
+                  <div className="logo-box">
+                    <img src={box.logo} alt="" />
+                  </div>
+                  <div>
+                    <h6>{box.name}</h6>
+                    <p>{box.desc}</p>
+                  </div>
                 </div>
-                <div>
-                  <h6>{box.name}</h6>
-                  <p>{box.desc}</p>
-                </div>
-              </div>
               ))}
-
             </div>
-         </div>
-
+          </div>
         </div>
 
-
-       <div className='bento-card thankyou-box'>
-       <PiHandsPrayingBold  size={32}/>
-        <h6>"Inspired by" <a href="https://www.perryw.ca/" target='_blank'>@perryw</a>, <a href="https://rauno.me/" target='_blank'>@rouno</a> and <a href="https://jrhu.me/" target='_blank'>@Jia</a> </h6>
-       </div>
-
+        <div className="bento-card thankyou-box">
+          <PiHandsPrayingBold size={32} />
+          <h6>
+            "Inspired by"{" "}
+            <a href="https://www.perryw.ca/" target="_blank">
+              @perryw
+            </a>
+            ,{" "}
+            <a href="https://rauno.me/" target="_blank">
+              @rouno
+            </a>{" "}
+            and{" "}
+            <a href="https://jrhu.me/" target="_blank">
+              @Jia
+            </a>{" "}
+          </h6>
+        </div>
       </motion.div>
 
-      <Footer/>
-      </motion.div>
-  )
-}
+      <Footer />
+    </motion.div>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,59 @@
-import React from 'react'
-import {motion} from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
-import Aurora from '../components/Aurora'
-import textVariants from '../static/textVariants'
-import childVariants from '../static/childVariants'
+import Aurora from "../components/Aurora";
+import textVariants from "../static/textVariants";
+import childVariants from "../static/childVariants";
 
 const Home = () => {
   return (
-    <motion.div variants={textVariants} initial='initial' animate='animate' className='flex justify-center items-center home-page overflow-hidden'>
-      <motion.div className='top-blob-archive'></motion.div>
-      <motion.div className='bot-blob-home'></motion.div>
-      <motion.div  variants={textVariants} initial='initial' animate='animate' className='hero-wrapper'>
-        <motion.div variants={childVariants} className='flex gap-[8px] '>
+    <motion.div
+      variants={textVariants}
+      initial="initial"
+      animate="animate"
+      className="flex justify-center items-center home-page overflow-hidden"
+    >
+      <motion.div className="top-blob-archive"></motion.div>
+      <motion.div className="bot-blob-home"></motion.div>
+      <motion.div
+        variants={textVariants}
+        initial="initial"
+        animate="animate"
+        className="hero-wrapper"
+      >
+        <motion.div variants={childVariants} className="flex gap-[8px] ">
           <h1>Hi, I'm Shubham,</h1>
-          <div className='fancy-image-box profile-img'></div>
+          <div className="fancy-image-box profile-img"></div>
         </motion.div>
-        <motion.div variants={childVariants}  className='flex gap-[8px] xm:gap-[8px]'>
-          <div className='fancy-image-box terminal'></div>
+        <motion.div
+          variants={childVariants}
+          className="flex gap-[8px] xm:gap-[8px]"
+        >
+          <div className="fancy-image-box terminal"></div>
           <h1>A Front-end</h1>
         </motion.div>
-        <motion.div variants={childVariants} className='flex gap-[8px] '>
+        <motion.div variants={childVariants} className="flex gap-[8px] ">
           <h1>Developer & Designer</h1>
-        <div className='fancy-image-box figma'></div>
+          <div className="fancy-image-box figma"></div>
         </motion.div>
-        <motion.div variants={childVariants} className='flex gap-[8px] '>
-          <div className='fancy-image-box mumbai'></div>
+        <motion.div variants={childVariants} className="flex gap-[8px] ">
+          <div className="fancy-image-box mumbai"></div>
           <h1>Based in Mumbai</h1>
         </motion.div>
-        <motion.div variants={childVariants} className='dotted-line'></motion.div>
-        <motion.p variants={childVariants}>Currently working at <span className='highlight-text-italic'>Artificial Reality</span> </motion.p>
-        <motion.p variants={childVariants}>This is where I share my work, projects, and things I’m learning along the way.</motion.p>
-
-
+        <motion.div
+          variants={childVariants}
+          className="dotted-line"
+        ></motion.div>
+        <motion.p variants={childVariants}>
+          Currently working at{" "}
+          <span className="highlight-text-italic">Artificial Reality</span>{" "}
+          <br />
+          This is where I share my work, projects, and things I’m learning along
+          the way.
+        </motion.p>
       </motion.div>
-
-      {/* <Aurora/> */}
     </motion.div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
