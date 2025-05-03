ceb0b65b24190de0ce3f85fd859d07324b7fb951
shubham <shubhamyelekar0@gmail.com>
2025-05-03 14:09:28 +0530

    Archive restructureing

--- src/pages/Archives.jsx
+++ src/pages/Archives.jsx
@@ -1,68 +1,108 @@
-import React, {useEffect, useState} from 'react'
-import {Link} from 'react-router-dom'
-import {motion} from 'framer-motion'
-import textVariants from '../static/textVariants';
-import childVariants from '../static/childVariants';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import textVariants from "../static/textVariants";
+import childVariants from "../static/childVariants";
 import { BiLinkExternal } from "react-icons/bi";
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
 import { useData } from "../DataContext"; //
 
-
 const Archives = () => {
-
-  const [archivePost , setArchivePost] = useState([])
-  const { data, loading} = useData();
+  const [archivePost, setArchivePost] = useState([]);
+  const { data, loading } = useData();
+  const majorProjects = archivePost.filter((item) => item.type === "major");
+  const minorProjects = archivePost.filter((item) => item.type === "mini");
 
   useEffect(() => {
-    if (data?.archive){
-      setArchivePost(data.archive)
+    if (data?.archive) {
+      setArchivePost(data.archive);
     }
+  }, [data]);
 
-  }, [data])
-
-  if (loading){
-    return <p>Loading</p>
+  if (loading) {
+    return <p>Loading</p>;
   }
 
   if (!archivePost.length) {
-    return <p>No Archives found</p>
+    return <p>No Archives found</p>;
   }
-  console.log(archivePost);
-  
   return (
-    <motion.div variants={textVariants} initial="initial" animate="animate" className='p-4'>
-      <motion.div className='top-blob-archive'></motion.div>
-      <motion.div className='top-grid-archive'></motion.div>
-      <motion.div variants={childVariants} initial="initial" animate="animate"  className='title-wrapper'>
-        <h3 className='text-center w-full'>Archives</h3>
-        <div className='dotted-line'></div>
+    <motion.div
+      variants={textVariants}
+      initial="initial"
+      animate="animate"
+      className="p-4"
+    >
+      <motion.div className="top-blob-archive"></motion.div>
+      <motion.div className="top-grid-archive"></motion.div>
+      <motion.div
+        variants={childVariants}
+        initial="initial"
+        animate="animate"
+        className="title-wrapper"
+      >
+        <h3 className="text-center w-full">Archives</h3>
+        <div className="dotted-line"></div>
       </motion.div>
-
-      <div className='archive-wrapper'>
-
-          {archivePost.map((items,index)=>(
-            <div key={index} className='archive-card'>
+      <div className="archive-wrapper">
+        <div className="archive-major-wrapper">
+          {majorProjects.map((items, index) => (
+            <div key={index} className="archive-card">
               {items.video ? (
-                <video autoPlay muted playsInline loop src={items.video} alt={items.name}></video>
+                <video
+                  autoPlay
+                  muted
+                  playsInline
+                  loop
+                  src={items.video}
+                  alt={items.name}
+                ></video>
               ) : (
                 <img src={items.thumbnail} alt={items.name} />
               )}
-              <div className='archive-info'>
+              <div className="archive-info">
                 <div>
-                <h5>{items.name}</h5>
-                <p>{items.description}</p>
+                  <h5>{items.name}</h5>
+                  <p>{items.description}</p>
                 </div>
-                <a href={items.link} target='_blank' className="link-button">
-                 <span>{items.linkType}</span>  <BiLinkExternal size={18} />
+                <a href={items.link} target="_blank" className="link-button">
+                  <span>{items.linkType}</span> <BiLinkExternal size={18} />
                 </a>
               </div>
             </div>
-
           ))}
+        </div>
+        <div className="archive-mini-wrapper">
+          {minorProjects.map((items, index) => (
+            <div key={index} className="archive-card">
+              {items.video ? (
+                <video
+                  autoPlay
+                  muted
+                  playsInline
+                  loop
+                  src={items.video}
+                  alt={items.name}
+                ></video>
+              ) : (
+                <img src={items.thumbnail} alt={items.name} />
+              )}
+              <div className="archive-info">
+                <div>
+                  <h5>{items.name}</h5>
+                  <p>{items.description}</p>
+                </div>
+                <a href={items.link} target="_blank" className="link-button">
+                  <span>{items.linkType}</span> <BiLinkExternal size={18} />
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </motion.div>
-  )
-}
+  );
+};
 
-export default Archives
+export default Archives;
