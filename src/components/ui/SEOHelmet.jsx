e3b04516adb1ce5be545e7971541716602ca20eb
Shubham Yelekar <70461738+Shubham-yelekar@users.noreply.github.com>
2025-06-12 23:09:24 +0530

    meta tags update

--- src/components/ui/SEOHelmet.jsx
+++ src/components/ui/SEOHelmet.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 
-const SEOHelmet = ({ title, description }) => {
+const SEOHelmet = ({ title, description, image }) => {
   return (
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
       <script src="https://cdn.jsdelivr.net/gh/eddymens/markdown-external-link-script@v2.0.0/main.min.js"></script>
+      <meta name="title" content="Shubham Yelekar | Front End Developer" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content="https://shubhamyelekar.vercel.app/blogs/space-time-complexity"
+      />
+      ''
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://shubhamyelekar.vercel.app/blogs/space-time-complexity"
+      />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
     </Helmet>
   );
 };
